74e9cdc47f3bf0ce9ad04a0257180d68146e1e9e
Fabiola <ftapara@unsa.edu.pe>
2023-02-01 23:41:25 -0500

    Primer intento de invertir filas por columnas

--- matrix.c
+++ matrix.c
@@ -8,26 +8,34 @@
 int m[INT_MAX][INT_MAX];
 
 void printMatrix(){
-    printf("matrix:\n");
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++)
-            printf("%d ", m[i][j]);
-        printf("\n");
-    }
+  printf("matrix:\n");
+  for(int i=0; i<N; i++){
+    for(int j=0; j<N; j++)
+      printf("%d ", m[i][j]);
+    printf("\n");
+  }
 }
 
 void generateMatrix(){
-    for(int i=0; i<N; i++)
-        for(int j=0; j<N; j++)
-            m[i][j] = RANDOM;
+  for(int i=0; i<N; i++)
+    for(int j=0; j<N; j++)
+      m[i][j] = RANDOM;
 }
 
 // hint 51: Try thinking about it layer by layer. Can you rotate a specific layer?.
 void easy_rotateMatrix(){
-    // here goes your code
+  printf("matrix rotate:\n");
+  for(int j=N; j>=0; j--){
+    //coloco j porque lo estoy invirtiendo
+    for(int i=0; i<N; i++)
+      printf("%d",m[i][j]);
+    printf("\n");
+  }
+  // here goes your code
+  
 }
 
 // hint 100: Rotating a specific layer would just mean swapping the values in four arrays. If you were asked to swap the values in two arrays, could you do this? Can you then extend it to four arrays?
 void hard_rotateMatrix(){
-    //here goes your code
+  //here goes your code
 }
